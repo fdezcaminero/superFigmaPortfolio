9e568456e9983b30f95c46ac19d4e7220e3fa6a9
José Fernández-Caminero <fdezcaminero@gmail.com>
2022-11-21 19:34:43 -0400

    Create function for JS mobile menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,21 @@
 function coolMenu() {
-    // let x = document.getElementsByTagName("body")[0];
-    // x.style.display = "none";
+    let x = document.getElementsByTagName("body")[0];
+    x.style.backgroundColor = "#e5e5e5";
 
   let webS = document.querySelector('#webSite');
   webS.style.display = "none";
 
   let mobileM = document.querySelector('#mobileMenu');
   mobileM.style.display = "block";
+}
+
+function closeMenu() {
+  let x = document.getElementsByTagName("body")[0];
+  x.style.backgroundColor = "#fff";
+
+  let webS = document.querySelector('#webSite');
+  webS.style.display = "block";
+
+  let mobileM = document.querySelector('#mobileMenu');
+  mobileM.style.display = "none";
 }
