281aeb67e4a3cd968a9eea0da9a30bb0a584b95a
José Fernández-Caminero <fdezcaminero@gmail.com>
2022-11-23 15:19:10 -0400

    Fix JS

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,12 @@ function closeMenu() {
   let mobileM = document.querySelector('#mobileMenu');
   mobileM.style.display = "none";
 }
+
+function projectMenu() {
+
+}
+
+
+
+// For each project you need to store the following pieces of data in a JavaScript object, 
+// at a minimum: name, description, featured image, technologies, link to live version, link to source.
